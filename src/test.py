08dbb78e8470a5d47825dd4dbe592508b8ca8358
Michal Pluta <vsdc48@durham.ac.uk>
2023-12-18 16:40:45 +0000

    Add test file to experiment with functionality

---
+++ src/test.py
@@ -0,0 +1,21 @@
+from prompt_toolkit import prompt
+import colorama
+from colorama import Fore, Style
+
+colorama.init()  # Initializes Colorama
+
+def get_user_input():
+    user_input = prompt("Enter your command: ", 
+                        # Add more features like auto-completion here
+                        )
+    return user_input
+
+def main():
+    while True:
+        command = get_user_input()
+        if command.lower() == 'exit':
+            break
+        print(Fore.GREEN + "You entered:" + Style.RESET_ALL, command)
+
+if __name__ == "__main__":
+    main()
