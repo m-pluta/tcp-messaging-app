bb77f6925f385cda5c98251e19d6d3eeaabf17b5
Michal Pluta <vsdc48@durham.ac.uk>
2023-12-23 20:08:15 +0000

    Improve generator and simplify code

--- src/utility.py
+++ src/utility.py
@@ -1,6 +1,5 @@
 import re
 
-
 def recv_to_buffer(socket, expected):
     total_data = b''
     received = 0
@@ -13,20 +12,16 @@ def recv_to_buffer(socket, expected):
 
     return total_data
 
-def recv_generator(socket, expected):
+def recv_generator(socket, expected, chunk_size=65536):
     received = 0
-    while received != expected:
-        print(received)
-        read_size = min(expected - received, 65536)
+    while received < expected:
+        read_size = min(expected - received, chunk_size)
         read_data = socket.recv(read_size)
-        yield read_data
+        if not read_data:
+            break
         received += len(read_data)
+        yield read_data
 
-
-def extract_delimiter(input):
-    # Using regular expression to extract data between <>
-    match = re.search(r'<(.*?)>', input)
-
-    if match:
-        return match.group(1)
-    return None
+def extract_delimiter(text):
+    match = re.search(r'<(.*?)>', text)
+    return match.group(1) if match else None
