d33c8141fac25688d7ec08c5665686a5e1a003f5
Michal Pluta <michalpl2003@gmail.com>
2023-12-26 23:10:40 +0000

    Make codebase simpler and add recv_generator method

--- src2/packet.py
+++ src2/packet.py
@@ -1,7 +1,8 @@
+from socket import socket
 from packet_type import PacketType
 
 HEADER_SIZE = 1024
-delimiter = '<###>'
+DELIMITER = '<###>'
 
 def encode_header(packet_type: PacketType, packet_size: int, **kwargs: dict):
     header_bytes = b''
@@ -9,21 +10,32 @@ def encode_header(packet_type: PacketType, packet_size: int, **kwargs: dict):
     header_bytes += str(packet_size).encode().ljust(16)
 
     for kwarg, value in kwargs.items():
-        text = f'{kwarg}:{str(value)}{delimiter}'
-        header_bytes += text.encode()
+        header_bytes += f'{kwarg}:{str(value)}{DELIMITER}'.encode()
 
     return header_bytes.ljust(HEADER_SIZE)
 
 
 def decode_header(data: bytes):
-    packet_type = PacketType(int(data[0:2].decode()))
+    packet_type = PacketType(int(data[:2].decode()))
     packet_size = int(data[2:18].decode())
 
-    encoded_values = data[18:].decode().rstrip().split(delimiter)[:-1]
+    encoded_values = data[18:].decode().rstrip().split(DELIMITER)[:-1]
     decoded_values = {}
-
     for value in encoded_values:
         key, val = value.split(':')
         decoded_values[key] = val
 
     return packet_type, packet_size, decoded_values
+
+
+# Reads the expected number of bytes from the socket in chunks.
+# Returns the bytes as a generator.
+# This means during download, the entire file isn't loaded into memory.
+def recv_generator(socket: socket, expected: int, chunk_size=65536):
+    received = 0
+    while received < expected:
+        read_data = socket.recv(min(chunk_size, expected - received))
+        if not read_data:
+            break
+        received += len(read_data)
+        yield read_data
