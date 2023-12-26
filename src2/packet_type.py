7cb28c5360ce2811660ab5cba84d5e2dce2ff33f
Michal Pluta <michalpl2003@gmail.com>
2023-12-26 20:32:23 +0000

    Add basic components

---
+++ src2/packet_type.py
@@ -0,0 +1,14 @@
+# Standard Library Imports
+from enum import Enum
+
+
+class PacketType(Enum):
+    METADATA = 1
+    OUT_MESSAGE = 2
+    IN_MESSAGE = 3
+    ANNOUNCEMENT = 4
+    FILE_LIST_REQUEST = 5
+    FILE_LIST = 6
+    DUPLICATE_USERNAME = 7
+    DOWNLOAD_REQUEST = 8
+    DOWNLOAD = 9
